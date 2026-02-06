09a9504fe205f87a827c8024cc7e352624b7313a
Carlos <cordaz@corsusa.com>
2026-02-05 23:51:52 -0500

    xd

--- src/app/upgrade/page.tsx
+++ src/app/upgrade/page.tsx
@@ -34,7 +34,7 @@ export default function UpgradePage() {
             const { data } = await api.payments.createProPayment();
 
             // Abrir en nueva ventana para evitar problemas con tarjetas guardadas
-            const checkoutUrl = data.data.initPoint || data.data.sandboxInitPoint;
+            const checkoutUrl = data.data.initPoint;
             const newWindow = window.open(checkoutUrl, '_blank');
             if (!newWindow) {
                 window.location.href = checkoutUrl;
